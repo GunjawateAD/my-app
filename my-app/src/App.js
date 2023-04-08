a1c1df462381b1385b947fd1184ac16239f0385a
Avishkar Dattatray Gunjawate <avishkargunjawate76@gmail.com>
2023-04-08 17:59:00 +0530

    all

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,19 +6,19 @@ import FocusInput from "./Componens/Hooks/FocusInput";
 import HookTimer from "./Componens/Hooks/HookTimer";
 import HightChecker from "./Componens/Hooks/HightChecker";
 import CounterOne from "./Componens/Hooks/CounterOne";
-import StopWatch from "./Componens/Hooks/StopWatch";
+import Home from "./Componens/Hooks/Home";
 
 function App() {
   return (
     <div className="App">
-      
+      <Home></Home>
       {/* <HExample1></HExample1> */}
       {/* <FocusInput></FocusInput>
       <HookTimer></HookTimer> */}
       {/* <HightChecker></HightChecker> */}
 
       {/* <CounterOne/> */}
-      <StopWatch/>
+      {/* <StopWatch/> */}
     </div>
   );
 }
